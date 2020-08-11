3f164022147f0ddf22f0ccf8f1b153233729c117
Amin <amin@lit.codes>
2020-08-11 17:39:29 +0800

    Force delete resources

--- k8s/delete.sh
+++ k8s/delete.sh
@@ -3,5 +3,5 @@
 kubectl patch pv read-receipt-db-pv -p '{"metadata":{"finalizers": []}}' --type=merge
 kubectl patch pvc read-receipt-db-pvc -p '{"metadata":{"finalizers": []}}' --type=merge
 for i in *.yaml; do
-    kubectl delete -f $i
+    kubectl delete --force -f $i
 done
