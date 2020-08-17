9cfb4fcc6fc598695e4e40f087ff7737c99a71de
Amin <amin@lit.codes>
2020-08-17 14:54:26 +0800

    Add installation of jenkins to create.sh

--- k8s/create.sh
+++ k8s/create.sh
@@ -8,5 +8,9 @@ kubectl apply -f local-storage.yaml
 kubectl apply -f read-receipt-secret.yaml
 kubectl apply -f read-receipt-db.yaml
 kubectl apply -f read-receipt.yaml
+./install_jenkins.sh
 kubectl apply -f load-balancer.yaml
+echo Application Load Balancer accessible from:
 kubectl get svc read-receipt-lb -o jsonpath='{.status.loadBalancer.ingress[0].hostname}'
+echo Jenkins Load Balancer accessible from:
+kubectl get svc cde-jenkins-lb -o jsonpath='{.status.loadBalancer.ingress[0].hostname}'
