8624d6159f5993cc21dc3ce57a0ba0ca02561562
Amin <amin@lit.codes>
2020-08-18 10:55:16 +0800

    Add blueocean plugins

--- k8s/create.sh
+++ k8s/create.sh
@@ -6,11 +6,11 @@ set -e
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm install cde-jenkins --values jenkins.yaml stable/jenkins
 JENKINS_POD=$(kubectl get pods --selector app.kubernetes.io/instance=cde-jenkins -o name | awk -F/ '{print $2}')
-kubectl wait --for=condition=ready pod $JENKINS_POD
+kubectl wait --timeout 180s --for=condition=ready pod $JENKINS_POD
 
 # Install kubectl on Jenkins
 curl -Lo /tmp/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18.8/bin/linux/amd64/kubectl
-kubectl cp /tmp/kubectl $JENKINS_POD:/opt/kubectl
+kubectl cp /tmp/kubectl $JENKINS_POD:/var/jenkins_home/kubectl
 
 # Install load balancers
 kubectl apply -f load-balancer.yaml
