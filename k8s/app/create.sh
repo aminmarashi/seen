c45961664931b4655d4315a8e750f29a2c2a2f69
Amin <amin@lit.codes>
2020-08-20 14:33:19 +0800

    Remove pvs from create

--- k8s/app/create.sh
+++ k8s/app/create.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-kubectl apply -f eks-nvme-ssd-provisioner.yaml
-kubectl apply -f storage-local-static-provisioner.yaml
-kubectl apply -f read-receipt-local-storage.yaml
-kubectl apply -f local-storage.yaml
 kubectl apply -f read-receipt-secret.yaml
 kubectl apply -f read-receipt-db.yaml
 kubectl apply -f read-receipt.yaml
