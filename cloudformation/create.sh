4c0d8e74b8b5e2fa9875543f6b1a3906a9a7571d
Amin <amin@lit.codes>
2020-08-20 15:02:00 +0800

    Wait 1 min before creating kubectl config

--- cloudformation/create.sh
+++ cloudformation/create.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 aws cloudformation create-stack --stack-name eks --template-body file://eks.yaml --parameters file://parameters.json --capabilities CAPABILITY_IAM
+sleep 60s
 ./kubeconfig.sh
