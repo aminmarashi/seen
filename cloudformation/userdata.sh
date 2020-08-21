123f9f3694908b1e63801f8b0b03126acd3efb68
Amin <amin@lit.codes>
2020-08-21 17:26:22 +0800

    Add docker installation

--- cloudformation/userdata.sh
+++ cloudformation/userdata.sh
@@ -6,13 +6,20 @@ set -e
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo apt-get install python3-pip -y
 sudo apt-get install openjdk-8-jre -y
 
 # Setup jenkins
 sudo apt-get install jenkins -y
-echo Waiting 15 minutes to make sure cluster is created, please do not stop the script
-sleep 15m
+echo Waiting 2 minute to make sure Jenkins is running
+sleep 1m
+sudo usermod -aG docker jenkins
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 jenkins_password=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 java -jar jenkins-cli.jar -s http://localhost:8080 -auth admin:$jenkins_password install-plugin kubernetes \
@@ -43,6 +50,8 @@ java -jar jenkins-cli.jar -s http://localhost:8080 -auth admin:$jenkins_password
                                                                                                 blueocean-commons
 
 # Setup k8s
+echo Waiting 13 minutes to make sure cluster is created, please do not stop the script
+sleep 14m
 sudo pip3 install awscli
 sudo -iu jenkins curl -L -o /var/lib/jenkins/kubectl "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo -iu jenkins chmod +x /var/lib/jenkins/kubectl
