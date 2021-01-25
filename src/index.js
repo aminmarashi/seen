b2c316e651e174d0bc72b137ab22c0e92f29afea
Amin <amin@lit.codes>
2021-01-25 22:46:02 +0800

    Fix password env

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const CONNINFO = {
   port: process.env.POSTGRES_PORT,
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASS,
+  password: process.env.POSTGRES_PASSWORD,
 };
 
 const db = pgPromise()(CONNINFO);
