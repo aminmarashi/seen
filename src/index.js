978f337bd38292ff02ef65977255827b444ebbac
Amin <66205764+ihsaram@users.noreply.github.com>
2021-01-30 22:36:49 +0800

    UI improvements (#18)
    * Better explanation when logged in, go to create after signup
    
    * Make things more informative and appealing
    
    * Rename stats to track, add emojis
    
    * Add a dropdown on the track page

--- src/index.js
+++ src/index.js
@@ -103,25 +103,27 @@ async function getAllStats(user_id) {
 
 app.get('/', attemptSilentLogin(), async (req, res) => {
   res.render('index', { 
-    title: 'Welcome to seen!',
+    title: 'Seen 👀',
     user: req.oidc.user,
   });
 });
 
-app.get('/stats', requiresAuth(), async (req, res) => {
+app.get('/track', requiresAuth(), async (req, res) => {
   const { email } = req.oidc.user;
   const user_id = await getOrCreateUser(email);
   const stats = await getAllStats(user_id);
-  
-  res.render('stats', {
-    title: 'Stats',
+  const receipts = await getAllReceipts(req.oidc.user.email);
+
+  res.render('track', {
+    title: 'Track a pixel🐕',
     user: req.oidc.user,
+    receipts,
     stats,
-    path: '/stats',
+    path: '/track',
   });
 });
 
-app.get('/stats/:receipt_id', requiresAuth(), async (req, res) => {
+app.get('/track/:receipt_id', requiresAuth(), async (req, res) => {
   const { receipt_id } = req.params;
 
   const receipt = await getReceiptById(receipt_id);
@@ -132,13 +134,13 @@ app.get('/stats/:receipt_id', requiresAuth(), async (req, res) => {
   const receipts = await getAllReceipts(req.oidc.user.email);
   const stats = await getReceiptStats(receipt.id);
 
-  res.render('stats', {
-    title: `Stats for ${receipt.name}`,
+  res.render('track', {
+    title: `Track ${receipt.name}🐕`,
     receipt,
     receipts,
     user: req.oidc.user, 
     stats,
-    path: `/stats`,
+    path: `/track`,
   });
 });
 
@@ -149,12 +151,12 @@ app.post('/create', requiresAuth(), async (req, res) => {
   const user_id = await getOrCreateUser(email);
   const receiptId = await createReceipt(user_id, receipt);
 
-  res.redirect(`/stats/${receiptId}`);  
+  res.redirect(`/track/${receiptId}`);  
 });
 
 app.get('/create', requiresAuth(), async (req, res) => {
   res.render('create', { 
-    title: 'Create a new email',
+    title: 'Create a new pixel 🕵️‍♀️',
     user: req.oidc.user, 
   });
 });
